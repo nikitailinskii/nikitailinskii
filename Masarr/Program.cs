3902bfbd0c68e19dc1d1dfd0edf07b616afb56bc
nikitailinskii <nikitailinskii80@gmail.com>
2023-02-22 10:43:24 +0200

    add cw

---
+++ Masarr/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Masarr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            byte n = byte.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+              for (int i = 0; i < array.Length; i++)
+              {
+                  Console.Write($"array[{i}]=");
+                  array.SetValue(int.Parse(Console.ReadLine()),i);
+              }
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine($"{array.GetValue(i)}");
+                }
+                Console.ReadKey();
+        }
+    }
+}
