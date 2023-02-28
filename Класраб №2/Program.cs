4d919eba411b750e79e2986acff35819e4f8f0a7
nikitailinskii <122911684+nikitailinskii@users.noreply.github.com>
2023-02-28 21:20:02 +0200

    work+

---
+++ Класраб №2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+namespace Класраб__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("n = ");
+            //Length свойство класса Array для оперделения размера о.м.
+            byte n = byte.Parse(ReadLine());
+            int[] arr = new int[n];
+
+            //Цикл для заполнения о.м с помощью метода SetValue.
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Write($"arr[{i}]=");
+                arr.SetValue(int.Parse(ReadLine()), i);
+            }
+
+            //Цикл вывода информации.
+            /*  for (int i = 0; i < arr.Length; i++)
+              {
+                  WriteLine($"{arr.GetValue(i)}"); 
+              }
+            WriteLine();
+            Array.Reverse(arr); */
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                WriteLine($"{arr.GetValue(i)}");
+            }
+            Write($"\n");
+            Array.Clear(arr, 2, 4);
+
+            foreach(int el in arr)
+            {
+                Write($"{el}\n");
+            }
+            ReadKey();
+        } 
+    }
+}
